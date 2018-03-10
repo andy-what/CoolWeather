eb72e8e7d219e03567294b6f9e91b605b300b549
andy <>
2018-03-10 13:13:36 +0800

    basic present

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by arch on 2018/3/9.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
